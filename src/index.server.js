1ee3e6c8e554f0f1fc2c8a7351426f7890f5ab87
lamvanhongUIT19520094 <19520094@gm.uit.edu.vn>
2022-05-27 13:21:20 +0700

    update db

--- src/index.server.js
+++ src/index.server.js
@@ -13,7 +13,8 @@ env.config();
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@ticketmanager-v2.uwkjp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    //n `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@ticketmanager-v2.uwkjp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.vj891.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
     console.log('Database connected');
@@ -31,13 +32,13 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, './uploads')));
 
+app.use('/api/enterprise', enterpriseRoutes);
+
 app.get('/', (req, res, next) => {
   const error = new AppError(`Error on this server`, 404);
 
   return next(error);
 });
-
-app.use('/api/enterprise', enterpriseRoutes);
 
 app.all('*', (req, res, next) => {
   const error = new AppError(`Can't find ${req.originalUrl} on this server`, 404);
