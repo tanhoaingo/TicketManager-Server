8035748812c3d33e2c0318aedc55317bf6a4a526
Nguyễn Ngọc Phúc <19520224@gm.uit.edu.vn>
2022-06-02 16:43:34 +0700

    Update API for Analytic

--- src/controllers/wagonTicket.js
+++ src/controllers/wagonTicket.js
@@ -2,6 +2,7 @@ const WagonTicket = require('../models/wagonTicket');
 const Trip = require('../models/trip.js');
 const Ticket = require('../models/ticket.js');
 const Seat = require('../models/seat.js');
+const user = require('../models/user');
 
 exports.create = async (req, res) => {
   const newWagonTicket = new WagonTicket(req.body);
@@ -80,6 +81,242 @@ exports.getSeatInWagon = async (req, res) => {
       });
       res.status(200).json(filteredSeats);
     }
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+exports.getTotalTicket_Sale = async (req, res) => {
+  try {
+    const { month, year } = req.body;
+
+    const result = await Seat.aggregate([
+      {
+        $lookup: {
+          from: 'wagontickets',
+          localField: 'idWagonTicket',
+          foreignField: '_id',
+          as: 'result',
+        },
+      },
+      {
+        $replaceRoot: {
+          newRoot: {
+            $mergeObjects: [
+              {
+                $arrayElemAt: ['$result', 0],
+              },
+              '$$ROOT',
+            ],
+          },
+        },
+      },
+      {
+        $project: {
+          result: 0,
+        },
+      },
+      {
+        $project: {
+          date: '$createdAt',
+          month: {
+            $month: '$createdAt',
+          },
+          year: {
+            $year: '$createdAt',
+          },
+          price: '$price',
+        },
+      },
+      {
+        $match: {
+          month: month,
+          year: year,
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $dateToString: {
+              format: '%Y-%m',
+              date: '$date',
+            },
+          },
+          totalTicket: {
+            $sum: 1,
+          },
+          totalSale: {
+            $sum: '$price',
+          },
+        },
+      },
+    ]);
+
+    const newUser = await user.aggregate([
+      {
+        $project: {
+          date: '$createdAt',
+          month: {
+            $month: '$createdAt',
+          },
+          year: {
+            $year: '$createdAt',
+          },
+        },
+      },
+      {
+        $match: {
+          month: month,
+          year: year,
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $dateToString: {
+              format: '%Y-%m',
+              date: '$date',
+            },
+          },
+          newUser: {
+            $sum: 1,
+          },
+        },
+      },
+      {
+        $sort: {
+          _id: 1,
+        },
+      },
+    ]);
+
+    var totalTicket = 0;
+    var totalSale = 0;
+    var totalNewUser = 0;
+
+    if (result.length != 0) {
+      totalTicket = result[0].totalTicket;
+      totalSale = result[0].totalSale;
+    }
+    // if (canceledTicket.length == 0) {
+    //   var totalCanceledTicket = 0;
+    // } else {
+    //   var totalCanceledTicket = canceledTicket[0].totalCanceledTicket;
+    // }
+    if (newUser.length != 0) {
+      totalNewUser = newUser[0].newUser;
+    }
+
+    res.status(200).json({ totalTicket, totalSale, totalNewUser });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+exports.getDateByMonthYear = async (req, res) => {
+  try {
+    const { month, year } = req.body;
+
+    const result = await Seat.aggregate([
+      {
+        $lookup: {
+          from: 'wagontickets',
+          localField: 'idWagonTicket',
+          foreignField: '_id',
+          as: 'result',
+        },
+      },
+      {
+        $replaceRoot: {
+          newRoot: {
+            $mergeObjects: [
+              {
+                $arrayElemAt: ['$result', 0],
+              },
+              '$$ROOT',
+            ],
+          },
+        },
+      },
+      {
+        $project: {
+          result: 0,
+        },
+      },
+      {
+        $project: {
+          date: '$createdAt',
+          month: {
+            $month: '$createdAt',
+          },
+          year: {
+            $year: '$createdAt',
+          },
+          price: '$price',
+        },
+      },
+      {
+        $match: {
+          month: month,
+          year: year,
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $dateToString: {
+              format: '%Y-%m-%d',
+              date: '$date',
+            },
+          },
+          totalTicket: {
+            $sum: 1,
+          },
+          totalSale: {
+            $sum: '$price',
+          },
+        },
+      },
+      {
+        $sort: {
+          _id: 1,
+        },
+      },
+    ]);
+    let listTicket = [];
+    let listSale = [];
+    var day = new Date(year, month, 0).getDate();
+
+    if (result.length == day) {
+      for (var ii = 0; ii < result.length; ii++) {
+        listTicket.push(result[ii].totalTicket);
+        listSale.push(result[ii].totalSale);
+      }
+    } else {
+      for (var i = 1, j = 0; i <= day; i++) {
+        if (j == result.length) {
+          listTicket.push(0);
+          listSale.push(0);
+        } else if (
+          result[j]._id ==
+          (i < 10
+            ? month < 10
+              ? `${year}-0${month}-0${i}`
+              : `${year}-${month}-0${i}`
+            : month < 10
+            ? `${year}-0${month}-${i}`
+            : `${year}-${month}-${i}`)
+        ) {
+          listTicket.push(result[j].totalTicket);
+          listSale.push(result[j].totalSale);
+          j++;
+        } else {
+          listTicket.push(0);
+          listSale.push(0);
+        }
+      }
+    }
+    res.status(200).json({ listTicket, listSale });
   } catch (err) {
     res.status(500).json({ error: err });
   }
--- src/routes/wagonTicket.js
+++ src/routes/wagonTicket.js
@@ -1,13 +1,24 @@
-const { create, getAll, getAllByIdTrip, getSeatInWagon } = require("../controllers/wagonTicket.js")
+const {
+  create,
+  getAll,
+  getAllByIdTrip,
+  getSeatInWagon,
+  getTotalTicket_Sale,
+  getDateByMonthYear,
+} = require('../controllers/wagonTicket.js');
 
-const router = require("express").Router();
+const router = require('express').Router();
 
-router.post("/create", create);
+router.post('/create', create);
 
-router.get("/", getAll);
+router.get('/', getAll);
 
-router.post("/getAllByIdTrip", getAllByIdTrip);
+router.post('/getAllByIdTrip', getAllByIdTrip);
 
-router.post("/getSeatInWagon", getSeatInWagon);
+router.post('/getSeatInWagon', getSeatInWagon);
+
+router.post('/getTotalTicket_Sale', getTotalTicket_Sale);
+
+router.post('/getDateByMonthYear', getDateByMonthYear);
 
 module.exports = router;
